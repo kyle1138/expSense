82aee2a054fff123047ea753b7e4c9c240bbf8e4
Kyle Hogan <kylehogan@Kyles-Air.home>
2015-04-18 14:20:52 -0400

    body check

--- expressServe.js
+++ expressServe.js
@@ -3,6 +3,7 @@ var twilio = require('twilio');
 var bodyParser = require('body-parser');
 var app = express();
 var msgArr = [];
+var reqB;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,14 +14,14 @@ app.get('/', function(request, response) {
   msgArr.forEach(function(msg){
     string += '<br>' + msg;
   })
-  response.send('Hello World!' + string);
+  response.send(reqB);
 });
 
 app.post('/sms', twilio.webhook({
     validate:false
 }), function(request, response) {
     console.log(request);
-    console.log(request[_readableState]);
+    reqB = request;
     console.log(request[body]);
     // Create a TwiML response
     var twiml = new twilio.TwimlResponse();
@@ -37,4 +38,5 @@ app.post('/sms', twilio.webhook({
 
 // Have express create an HTTP server that will listen on port 3000
 // or "process.env.PORT", if defined
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8080);
+// app.listen(3000);
