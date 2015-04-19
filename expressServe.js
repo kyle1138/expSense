60edc9fcf0d226ee6c28594ffaabf9fe5e3dfd6d
Kyle Hogan <kylehogan@Kyles-Air.home>
2015-04-19 14:36:44 -0400

    body check

--- expressServe.js
+++ expressServe.js
@@ -2,6 +2,8 @@ var express = require('express');
 var twilio = require('twilio');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("sense.db");
 var app = express();
 var msgArr = [];
 var reqB;
@@ -21,12 +23,34 @@ app.get('/', function(request, response) {
 app.post('/sms', twilio.webhook({
     validate:false
 }), function(request, response) {
-    console.log(request);
+
+  var rPhone = request['body']['From'];
+  var rBody = request['body']['Body'];
+
+
+
+  db.get("SELECT * FROM users WHERE phone = ?", rPhone, function(err, row) {
+    if(row){
+      db.run("INSERT INTO messages (message,phone) VALUES(?,?)" , rBody, rPhone, function(err){});
+    }else{
+    db.run("INSERT INTO users (phone) VALUES (?)", rPhone, function(err) {
+    if(err) { throw err; }
+    // var id = this.lastID; //weird way of getting id of what you just inserted
+    db.run("INSERT INTO messages (message,phone) VALUES(?,?)" , rBody, rPhone, function(err) {
+      if(err) { throw err; }
+
+    });
+  });
+}
+
+
+});
+
     msgArr.push(request['body']['Body']);
-    console.log(request['body']['Body']);
+    console.log(request['body']);
     // Create a TwiML response
     var twiml = new twilio.TwimlResponse();
-    twiml.message('Hello from Heroku node.js!');
+    twiml.message('Hello from express node.js!');
 
     // Render the TwiML response as XML
     fs.writeFile("req.txt", reqB, function (err) {
