ce999ee1066314a8617230eeda977bec421d431a
Kyle Hogan <kylehogan@Kyles-Air.home>
2015-04-19 20:58:24 -0400

    api adjusted

--- expressServe.js
+++ expressServe.js
@@ -90,7 +90,7 @@ app.post('/sms', twilio.webhook({
 
 app.post('/operator', function(request, response) {
 
-
+  console.log(request);
   var sent = request;
   db.run("INSERT INTO messages (body,phone,received) VALUES(?,?,?)" , sent['body'], sent['phone'],false, function(err) {
     if(err) { throw err; }
