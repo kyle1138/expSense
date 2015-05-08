cf8974ce8ff2f6b3b325829ab7825f72d3c19ed0
Kyle Hogan <kylehogan1980@yahoo.com>
2015-05-08 19:52:31 -0400

    multi message timeout

--- expressServe.js
+++ expressServe.js
@@ -114,42 +114,10 @@ server.on("connection" , function(ws){
 
 
 
-  // end post in websocket
 
 
 
-  // ws.on("message" , function(msg){
-  //
-  //       msgOut = msg;
-  //       console.log(msg);
-  //
-  //
-  //   client.sms.messages.create({
-  //       to:'+15165781916',
-  //       from:'2132973673',
-  //       body:msgOut
-  //   }, function(error, message) {
-  //       // The HTTP request to Twilio will run asynchronously. This callback
-  //       // function will be called when a response is received from Twilio
-  //       // The "error" variable will contain error information, if any.
-  //       // If the request was successful, this value will be "falsy"
-  //       if (!error) {
-  //           // The second argument to the callback will contain the information
-  //           // sent back by Twilio for the request. In this case, it is the
-  //           // information about the text messsage you just sent:
-  //           console.log('Success! The SID for this SMS message is:');
-  //           console.log(message.sid);
-  //
-  //           console.log('Message sent on:');
-  //           console.log(message.dateCreated);
-  //       } else {
-  //           console.log('Oops! There was an error.');
-  //           console.log(error);
-  //       }
-  //   });
 
-
-  })
 
   ws.on("close" , function(){
     var escapee = clients.indexOf(ws);
@@ -213,7 +181,7 @@ app.post('/operator', function(request, response) {
 
 
 //
-// var accountSid = 'AC5ef8732a3c49700934481addd5ce1659';
+// var accountSid = ;
 // var authToken = "{{ auth_token }}";
 // var clientBig = require('twilio')(tas, tat);
 //
